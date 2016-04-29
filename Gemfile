45d889453ec262c684008ff94c687541a946e326
Jay Patel <jpjaypatel34@gmail.com>
2016-04-29 16:54:53 +1200

    Add and configure acts_as_votable

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'devise', '~> 4.0', '>= 4.0.1'
 gem 'paperclip', '~> 4.3', '>= 4.3.6'
 gem 'masonry-rails', '~> 0.2.4'
+gem 'acts_as_votable', '~> 0.10.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :find_pin, only: [:show, :edit, :update, :destroy, :upvote]
 
 	def index
 		@pins = Pin.all.order("created_at DESC")
@@ -35,6 +35,10 @@ class PinsController < ApplicationController
 	def destroy
 		@pin.destroy
 		redirect_to root_path
+	end
+	def upvote
+		@pin.upvote_by current_user
+		redirect_to :back
 	end
 
 	private
